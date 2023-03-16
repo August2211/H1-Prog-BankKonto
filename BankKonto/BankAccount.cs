b460a4bcd556399abd0f4f375c693390f6f49091
August2211 <53059031+August2211@users.noreply.github.com>
2023-03-16 10:06:16 +0100

    Add project files.

---
+++ BankKonto/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankKonto
+{
+    public class BankAccount
+    {
+        private int Balance { get; set; }
+
+        public int GetBalance()
+        {
+            return Balance;
+        }
+
+        public int withdraw(int amount)
+        {
+            if(amount > Balance) return 0;
+            if (amount < 0) return 0;
+            Balance -= amount;            
+            return amount;         
+        }
+
+        public void deposit(int amount)
+        {
+            if (amount < 0) return;
+            Balance += amount;
+            return;
+        }
+    }
+}
---
+++ BankKonto/Program.cs
@@ -0,0 +1,40 @@
+﻿using BankKonto;
+
+BankAccount account = new BankAccount();
+
+Console.WriteLine("Withdraw\nDeposit\nGetBalance");
+
+while (true)
+{
+
+    switch (Console.ReadLine().ToLower())
+    {
+        case "withdraw":
+            {
+                Console.WriteLine("Withdraw amount:");
+                int.TryParse(Console.ReadLine(), out int amount);
+                account.withdraw(amount);
+                break;
+            }
+        case "deposit":
+            {
+                Console.WriteLine("Deposit amount:");
+                int.TryParse(Console.ReadLine(), out int amount);
+                account.deposit(amount);
+                break;
+            }
+        case "getbalance":
+            {
+                Console.WriteLine("Current balance: " + account.GetBalance());
+                break;
+            }
+    }
+}
+
+//Console.WriteLine(account.GetBalance());
+//account.deposit(100);
+//Console.WriteLine(account.GetBalance());
+//account.withdraw(50);
+//Console.WriteLine(account.GetBalance());
+//account.withdraw(100);
+//Console.WriteLine(account.GetBalance());
